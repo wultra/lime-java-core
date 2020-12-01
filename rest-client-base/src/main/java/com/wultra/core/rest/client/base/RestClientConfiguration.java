a08592e0aec7fef633630997dfcbe8db8d7f7dbe
Roman Strobl <roman.strobl@wultra.com>
2020-12-01 13:27:08 +0100

    Add JavaDoc

--- rest-client-base/src/main/java/com/wultra/core/rest/client/base/RestClientConfiguration.java
+++ rest-client-base/src/main/java/com/wultra/core/rest/client/base/RestClientConfiguration.java
@@ -24,6 +24,10 @@ import org.springframework.http.MediaType;
  */
 public class RestClientConfiguration {
 
+    /**
+     * Constructor of REST client configuration.
+     * @param baseUrl Base URL for all requests.
+     */
     public RestClientConfiguration(String baseUrl) {
         this.baseUrl = baseUrl;
     }
@@ -54,110 +58,218 @@ public class RestClientConfiguration {
     private String httpBasicAuthUsername;
     private String httpBasicAuthPassword;
 
+    /**
+     * Get base URL.
+     * @return Base URL.
+     */
     public String getBaseUrl() {
         return baseUrl;
     }
 
+    /**
+     * Get content type.
+     * @return Content type.
+     */
     public MediaType getContentType() {
         return contentType;
     }
 
+    /**
+     * Set content type.
+     * @param contentType Content type.
+     */
     public void setContentType(MediaType contentType) {
         this.contentType = contentType;
     }
 
+    /**
+     * Get accept type.
+     * @return Accept type.
+     */
     public MediaType getAcceptType() {
         return acceptType;
     }
 
+    /**
+     * Set accept type.
+     * @param acceptType Accepty type.
+     */
     public void setAcceptType(MediaType acceptType) {
         this.acceptType = acceptType;
     }
 
+    /**
+     * Get whether proxy is enabled.
+     * @return Whether proxy is enabled.
+     */
     public boolean isProxyEnabled() {
         return proxyEnabled;
     }
 
+    /**
+     * Set whether proxy is enabled.
+     * @param proxyEnabled Whether proxy is enabled.
+     */
     public void setProxyEnabled(boolean proxyEnabled) {
         this.proxyEnabled = proxyEnabled;
     }
 
+    /**
+     * Get proxy host.
+     * @return Proxy host.
+     */
     public String getProxyHost() {
         return proxyHost;
     }
 
+    /**
+     * Set proxy host.
+     * @param proxyHost Proxy host.
+     */
     public void setProxyHost(String proxyHost) {
         this.proxyHost = proxyHost;
     }
 
+    /**
+     * Get proxy port.
+     * @return Proxy port.
+     */
     public int getProxyPort() {
         return proxyPort;
     }
 
+    /**
+     * Set proxy port.
+     * @param proxyPort Proxy port.
+     */
     public void setProxyPort(int proxyPort) {
         this.proxyPort = proxyPort;
     }
 
+    /**
+     * Get proxy username.
+     * @return Proxy username.
+     */
     public String getProxyUsername() {
         return proxyUsername;
     }
 
+    /**
+     * Set proxy username.
+     * @param proxyUsername Proxy username.
+     */
     public void setProxyUsername(String proxyUsername) {
         this.proxyUsername = proxyUsername;
     }
 
+    /**
+     * Get proxy password.
+     * @return Proxy password.
+     */
     public String getProxyPassword() {
         return proxyPassword;
     }
 
+    /**
+     * Set proxy password.
+     * @param proxyPassword Proxy password.
+     */
     public void setProxyPassword(String proxyPassword) {
         this.proxyPassword = proxyPassword;
     }
 
+    /**
+     * Get connection timeout.
+     * @return Connection timeout.
+     */
     public Integer getConnectionTimeout() {
         return connectionTimeout;
     }
 
+    /**
+     * Set connection timeout.
+     * @param connectionTimeout Connection timeout.
+     */
     public void setConnectionTimeout(Integer connectionTimeout) {
         this.connectionTimeout = connectionTimeout;
     }
 
+    /**
+     * Get whether invalid SSL certificate is accepted.
+     * @return Whether invalid SSL certificate is accepted.
+     */
     public boolean isAcceptInvalidSslCertificate() {
         return acceptInvalidSslCertificate;
     }
 
+    /**
+     * Set whether invalid SSL certificate is accepted.
+     * @param acceptInvalidSslCertificate Whether invalid SSL certificate is accepted.
+     */
     public void setAcceptInvalidSslCertificate(boolean acceptInvalidSslCertificate) {
         this.acceptInvalidSslCertificate = acceptInvalidSslCertificate;
     }
 
+    /**
+     * Get maximum in memory request size.
+     * @return Maximum in memory request size.
+     */
     public Integer getMaxInMemorySize() {
         return maxInMemorySize;
     }
 
+    /**
+     * Set maximum in memory request size.
+     * @param maxInMemorySize Maximum in memory request size.
+     */
     public void setMaxInMemorySize(Integer maxInMemorySize) {
         this.maxInMemorySize = maxInMemorySize;
     }
 
+    /**
+     * Get whether basic HTTP authentication is enabled.
+     * @return Whether basic HTTP authentication is enabled.
+     */
     public boolean isHttpBasicAuthEnabled() {
         return httpBasicAuthEnabled;
     }
 
+    /**
+     * Set whether basic HTTP authentication is enabled.
+     * @param httpBasicAuthEnabled Whether basic HTTP authentication is enabled.
+     */
     public void setHttpBasicAuthEnabled(boolean httpBasicAuthEnabled) {
         this.httpBasicAuthEnabled = httpBasicAuthEnabled;
     }
 
+    /**
+     * Get username for basic HTTP authentication.
+     * @return Username for basic HTTP authentication.
+     */
     public String getHttpBasicAuthUsername() {
         return httpBasicAuthUsername;
     }
 
+    /**
+     * Set username for basic HTTP authentication.
+     * @param httpBasicAuthUsername Username for basic HTTP authentication.
+     */
     public void setHttpBasicAuthUsername(String httpBasicAuthUsername) {
         this.httpBasicAuthUsername = httpBasicAuthUsername;
     }
 
+    /**
+     * Get password for basic HTTP authentication.
+     * @return Password for basic HTTP authentication.
+     */
     public String getHttpBasicAuthPassword() {
         return httpBasicAuthPassword;
     }
 
+    /**
+     * Set password for basic HTTP authentication.
+     * @param httpBasicAuthPassword Password for basic HTTP authentication.
+     */
     public void setHttpBasicAuthPassword(String httpBasicAuthPassword) {
         this.httpBasicAuthPassword = httpBasicAuthPassword;
     }
